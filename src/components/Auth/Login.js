c427d4cdd3f915d8c3c4dcfae6d9059d22e27eca
Steve Marquez <steveantonio00@gmail.com>
2020-10-26 15:26:49 -0700

    Implement login functionality w/ firebase

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,121 @@
 import React, { Component } from "react";
+import firebase from "../../firebase";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon,
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 export default class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+    errors: [],
+    loading: false,
+  };
+
+  displayError = (errors) =>
+    errors.map((err, i) => <p key={i}>{err.message}</p>);
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = this.state;
+
+    if (this.isFormValid(this.state)) {
+      this.setState({ errors: [], loading: true });
+
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then((signedInUser) => {
+          console.log(signedInUser);
+        })
+        .catch((err) => {
+          console.error(err);
+          this.setState({
+            errors: this.state.errors.concat(err),
+            loading: false,
+          });
+        });
+    }
+  };
+
+  isFormValid = ({ email, password }) => {
+    return email && password;
+  };
+
+  handleInputError = (errors, inputName) => {
+    return errors.some((error) =>
+      error.message.toLowerCase().includes(inputName)
+    )
+      ? "error"
+      : "";
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, errors, loading } = this.state;
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h1" icon color="violet" textAlign="center">
+            <Icon name="code branch" color="violet" />
+            Login to Devchat
+          </Header>
+          <Form size="large" onSubmit={this.handleSubmit}>
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="email"
+                onChange={this.handleChange}
+                type="email"
+                value={email}
+                className={this.handleInputError(errors, "email")}
+              />
+              <Form.Input
+                fluid
+                name="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="password"
+                onChange={this.handleChange}
+                type="password"
+                value={password}
+                className={this.handleInputError(errors, "password")}
+              />
+              <Button
+                className={loading ? "loading" : ""}
+                color="violet"
+                fluid
+                size="large"
+                disabled={loading}
+              >
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          {errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayError(errors)}
+            </Message>
+          )}
+          <Message>
+            Don't have an account? ?<Link to="/register"> Register </Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
