04c9647f30ca4f6f9b7197c81d76fa2acebda770
Steve Marquez <steveantonio00@gmail.com>
2020-10-26 19:46:34 -0700

    Implement redux store and loader

--- src/index.js
+++ src/index.js
@@ -1,24 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
 
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
+import Spinner from "./Spinner";
 
 import registerServiceWorker from "./registerServiceWorker";
+import firebase from "./firebase";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers/index";
+import { setUser } from "./actions/index";
+// Global State
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends Component {
+  componentDidMount() {
+    console.log(this.props.isLoading);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // setUser(user)
+        this.props.setUser(user);
+        this.props.history.push("/");
+      }
+    });
+  }
+  render() {
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const mapStateFromProps = (state) => ({
+  isLoading: state.user.isLoading,
+});
+
+// we use this to redirect component to our main root
+const RootWithAuth = withRouter(connect(mapStateFromProps, { setUser })(Root));
+
+ReactDOM.render(
+  // Provide global state to whoever component wants to use it
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
-
-ReactDOM.render(<Root />, document.getElementById("root"));
 registerServiceWorker();
