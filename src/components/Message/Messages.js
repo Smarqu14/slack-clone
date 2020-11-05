14cbc5ec551a26cfc83983c35f16353b4d6e24ce
Steve Marquez <steveantonio00@gmail.com>
2020-11-05 14:49:55 -0800

    Display added comment on the Messageform component

--- src/components/Message/Messages.js
+++ src/components/Message/Messages.js
@@ -1,28 +1,70 @@
 import React, { Component, Fragment } from "react";
 import { Segment, Comment } from "semantic-ui-react";
+import firebase from "../../firebase";
+
 import MessageHeader from "./MessageHeader";
 import MessageForm from "./MessageForm";
-import firebase from "../../firebase";
+import Message from "./Message";
 
 export default class Messages extends Component {
   state = {
     messagesRef: firebase.database().ref("messages"),
     channel: this.props.currentChannel,
-    currentUser: this.props.currentUser,
+    user: this.props.currentUser,
+    messages: [],
+    messagesLoading: true,
+  };
+
+  componentDidMount() {
+    const { channel, user } = this.state;
+
+    if (channel && user) {
+      this.addListeners(channel.id);
+    }
+  }
+
+  addListeners = (channelId) => {
+    this.addMessageListener(channelId);
+  };
+
+  displayMessages = (messages) => {
+    return (
+      messages.length > 0 &&
+      messages.map((message) => (
+        <Message
+          key={message.timestamp}
+          message={message}
+          user={this.state.user}
+        />
+      ))
+    );
+  };
+
+  addMessageListener = (channelId) => {
+    let loadedMessages = [];
+    this.state.messagesRef.child(channelId).on("child_added", (snap) => {
+      loadedMessages.push(snap.val());
+      this.setState({
+        messages: loadedMessages,
+        messagesLoading: false,
+      });
+    });
   };
   render() {
-    const { messagesRef, channel, currentUser } = this.state;
+    const { messagesRef, messages, channel, user } = this.state;
 
     return (
       <Fragment>
         <MessageHeader />
         <Segment>
-          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+          <Comment.Group className="messages">
+            {this.displayMessages(messages)}
+          </Comment.Group>
         </Segment>
         <MessageForm
           messagesRef={messagesRef}
           currentChannel={channel}
-          currentUser={currentUser}
+          currentUser={user}
         />
       </Fragment>
     );
